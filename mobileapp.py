e714172c957aafca00fd2098a0e3b7cb0b65f762
mauricioxpereira <106484622+mauricioxpereira@users.noreply.github.com>
2022-08-14 14:57:28 -0400

    Update mobileapp.py

--- mobileapp.py
+++ mobileapp.py
@@ -1,6 +1,3 @@
-# cd Users\MP\OneDrive - Miami Dade College\7-Summer2022\Internships\SRI\application>py kivycourse.py
-#change font is on time stamp: 1:18:12 of 5:41:25 (custom font) it is a cool one
-
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
@@ -8,32 +5,54 @@ from kivy.uix.button import Button
 from kivy.properties import StringProperty
 import socketio
 from time import sleep
-#class BozLayoutExample(BowxLayout):
+#class BoxLayoutExample(BowxLayout):
 #	def __init__(self, **kwargs):
 
+#____________________________________________________________________
+sio = socketio.Client()
 
+@sio.event
+def connect():
+    print('Connection established with server to send message data.')
+
+def send_msg(msg):
+    print("sending")
+    sio.emit('msg', msg)
+
+@sio.event
+def disconnect():
+    print('Disconnected from websocket! Cannot send message data.')
+
+sio.connect('ws://localhost:3000')
+#___________________________________________________________________
 class MainWidget(Widget):
 	my_text=StringProperty("hi!")
-	def on_buttonup_click(self):	
+	def on_buttonup_click(self):
+		send_msg("Button up clicked")	
 		print("Button up clicked")
 		
-		self.my_text= "You clicked, so I send a string to the app"
+		#self.my_text= "You clicked, so I send a string to the app"
 	
 	count=0
 	count_text = StringProperty("0")
 	def on_buttondown_click(self):
+		send_msg("Button down clicked")
 		print("Button down clicked")
-		self.count+= 1
-		self.count_text=str(self.count)
+		#self.count+= 1
+		#self.count_text=str(self.count)
 		
 
 	def on_buttonleft_click(self):
+		send_msg("Button left clicked")
 		print("Button left clicked")
 
 	def on_buttonright_click(self):
+		send_msg("Button right clicked")
 		print("Button right clicked")
 		
 	def on_buttonhome_click(self):
+		send_msg("Button home clicked")
+		sio.disconnect()
 		print("Button home clicked")
 
 	#toggle button function
